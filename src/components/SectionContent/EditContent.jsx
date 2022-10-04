61c44d0b0c4bbbab321a2f3631bd3a2670d670a1
leehaneul623 <dyj03216@naver.com>
2022-10-04 17:33:15 +0900

    글 작성 수정 페이지 구현

--- src/components/SectionContent/EditContent.jsx
+++ src/components/SectionContent/EditContent.jsx
@@ -6,7 +6,7 @@ const EditContent = () => {
     <div className="w-[1200px] h-[1187px] pt-20">
       <div className="flex flex-row ml-32 mt-16 text-xl">
         <BsChatDots />
-        <p className="ml-4">자유롭게 작성해주세요.</p>
+        <p className="ml-4">자유롭게 수정해주세요.</p>
       </div>
       <div className="flex items-center w-[1024px] h-[75px] bg-white rounded-3xl ml-32 mt-10">
         <p className="text-lg text-gray-400 ml-8">제목을 입력해주세요.</p>
