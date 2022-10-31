013ac34dbe0974e2cac3b580c1d43a759a20b7d1
leehaneul623 <dyj03216@naver.com>
2022-10-31 13:22:02 +0900

    아이콘 hover

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -36,25 +36,43 @@ const Header = () => {
             </button>
           </form>
         </div>
-        <div className="flex flex-row justify-end items-center w-[150px] h-[60px] mr-8">
-          {user == null ? (
-            <button
-              onClick={() => {
-                navigate('/login')
-              }}
-            >
-              <BsBoxArrowInRight className="text-[28px] text-gray-500" />
-            </button>
-          ) : (
-            ''
-          )}
-          <Link to="/signup">
-            <BsPersonPlusFill className="text-[28px] text-gray-500 ml-6" />
-          </Link>
-          <Link to="/write">
-            <BiEditAlt className="text-[28px] text-gray-500 ml-6" />
-          </Link>
-        </div>
+
+        <ul className="flex flex-row justify-end items-center w-[150px] h-[60px] mr-8">
+          <li className="group relative h-[28px]">
+            {user == null ? (
+              <button
+                onClick={() => {
+                  navigate('/login')
+                }}
+              >
+                <BsBoxArrowInRight className="text-[28px] text-gray-500" />
+                <span className="absolute top-15 left-0 text-gray-400 text-lg hidden group-hover:block">
+                  Login
+                </span>
+              </button>
+            ) : (
+              ''
+            )}
+          </li>
+
+          <li className="group relative h-[28px]">
+            <Link to="/signup">
+              <BsPersonPlusFill className="text-[28px] text-gray-500 ml-6" />
+              <span className="absolute top-15 left-3 text-gray-400 text-lg hidden group-hover:block">
+                SignUp
+              </span>
+            </Link>
+          </li>
+
+          <li className="group relative h-[28px]">
+            <Link to="/write">
+              <BiEditAlt className="text-[28px] text-gray-500 ml-6" />
+              <span className="absolute top-15 left-3 text-gray-400 text-lg hidden group-hover:block">
+                Write
+              </span>
+            </Link>
+          </li>
+        </ul>
       </div>
     </div>
   )
